fe8f671065be7972725abb4aae0932c44a89834e
Dilki30 <as20240479@sci.sjp.ac.lk>
2026-07-20 11:07:27 +0530

    Added Supabase API keys

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
 // Replace these with your actual Supabase project credentials
-const supabaseUrl = 'YOUR_SUPABASE_PROJECT_URL'
-const supabaseKey = 'YOUR_SUPABASE_ANON_KEY'
+const supabaseUrl = 'https://tznkbyaoicklmxollgzh.supabase.co/rest/v1/'
+const supabaseKey = 'sb_publishable_zS_PVyN7nnX00sqjVg-5oA_Wdn3jLoV'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
